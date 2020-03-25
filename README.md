fd899a3e2cc63aaa921c6d5fe14e38f096462e5a
viper_work <wiperawa@gmail.com>
2020-03-25 18:21:20 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # yii2-see-more-widget
 widget that cut off html content to predefined height, and display ...see more link
+
+## Installation
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+$ php composer.phar require wiperawa/yii2-see-more-widget "dev-master"
+```
+
+or add
+
+```
+"wiperawa/yii2-copy-to-clipboard-widget": "dev-master"
+```
+
+to the ```require``` section of your `composer.json` file.
+
+## Usage
+```
+use \wiperawa\seemorewidget\SeeMoreWidget;
+
+SeeMoreWidget::begin([
+    'height' => 160 //height of cutoff container in pixels
+    //'containerOptions' => [], //Html attributes of container
+    //'btnOptions => [], //Html attributes of buttons
+    //'showMoreLabel' => '...(see more)',
+    //'showLessLabel' => '(less)',
+    //'opened' => false //Show allready opened
+    //''
+]);
+    ....Some HTML goes here....
+    ...........................
+    ...........................
+SeeMoreWidget::end()
+
+
+
+```
---
+++ SeeMoreWidget.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace common\widgets;
+
+use Yii;
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\web\JsExpression;
+use yii\helpers\ArrayHelper;
+use yii\i18n\PhpMessageSource;
+
+/**
+ * SeeMoreWidget to cut off some big text/html, and add 'See  more' link to expand complete text .
+ * usage:
+ * SeeMoreWidget::begin();
+ * ....Some html goes here...
+ * SeeMoreWidget::end();
+ *
+ * Class SeeMoreWidget
+ * @package common\widgets
+ */
+class SeeMoreWidget extends Widget
+{
+
+    /**
+     * Html options for content container
+     * @var array
+     */
+    public $containerOptions = [];
+
+    /**
+     * Buttons html options
+     * @var array
+     */
+    public $btnOptions = [
+        'style' => [
+            'margin' => '10px 0',
+        ]
+    ];
+
+    /**
+     * Rendered state, container allready open or not.
+     * @var bool
+     */
+    public $opened = false;
+
+    /**
+     * shorten container height
+     * @var int
+     */
+    public $containerHeight = 160;
+
+    /**
+     * Show label text
+     * @var string
+     */
+    public $showMoreLabel = '...(see more)';
+
+    /**
+     * less label text
+     * @var string
+     */
+    public $showLessLabel = '(less)';
+
+
+    /**
+     * Default content container options
+     * @var array
+     */
+    protected $defaultContainerOptions = [
+        'style' => [
+            'overflow' => 'hidden',
+        ],
+    ];
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function init()
+    {
+        parent::init();
+        ob_start();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return string|void
+     */
+    public function run()
+    {
+
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        $moreBtnId = $this->getId() . '-more-btn';
+        $lessBtnId = $this->getId() . '-less-btn';
+        $containerId = $this->getId() . '-container';
+        $lessBtnLabel = Yii::t($this->_msgCategory, $this->showLessLabel);
+        $moreBtnLabel = Yii::t($this->_msgCategory, $this->showMoreLabel);
+
+        if (!$this->opened) {
+            $this->defaultContainerOptions['style']['max-height'] = $this->containerHeight . 'px';
+        }
+
+        echo Html::tag(
+            'div',
+            $content,
+            ArrayHelper::merge(
+                ['id' => $containerId],
+                $this->containerOptions,
+                $this->defaultContainerOptions)
+        );
+
+        echo Html::a($moreBtnLabel, '', ArrayHelper::merge($this->btnOptions, [
+            'style' => [
+                'display' => 'none',
+            ],
+            'id' => $moreBtnId,
+            'onClick' => new JsExpression("
+                $('#{$containerId}').css({'max-height': ''}); 
+                $('#{$lessBtnId}').show();$(this).hide(); 
+                return false; 
+            ")
+        ]));
+        echo Html::a($lessBtnLabel, '#', ArrayHelper::merge($this->btnOptions, [
+            'style' => [
+                'display' => 'none',
+            ],
+            'id' => $lessBtnId,
+            'onClick' => new JsExpression("
+                $('#{$containerId}').css({'max-height': '{$this->containerHeight}px'}); 
+                $('#{$moreBtnId}').show();
+                $(this).hide();
+            ")
+        ]));
+
+        $opened = json_encode($this->opened);
+
+        $this->getView()->registerJs("
+            if ( $('#{$containerId}').height() >= {$this->containerHeight}) { 
+                if ({$opened} == true) {
+                    $('#{$lessBtnId}').show();
+                } else {
+                    $('#{$moreBtnId}').show();
+                }
+            } 
+        ");
+    }
+
+
+}
